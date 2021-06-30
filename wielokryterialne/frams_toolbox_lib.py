5d8c3d81c23a0f3980f76e424c67c2bc3640a02d
Domin <domin.dober@gmail.com>
2021-06-30 13:31:13 +0200

    Add frams toolbox

---
+++ wielokryterialne/frams_toolbox_lib.py
@@ -0,0 +1,34 @@
+#  benchmarks
+from deap import creator, base, tools
+
+from framsticks.new_frams.FramsticksLib import FramsticksLib
+
+
+def register_standard(attributes, creator, cli):
+    toolbox = base.Toolbox()
+
+    toolbox.register("attr_frams", cli.getSimplest, '1')
+    toolbox.register("individual", tools.initRepeat, creator.Individual,
+                     toolbox.attr_frams, attributes)
+    toolbox.register("population", tools.initRepeat, list, toolbox.individual)
+
+    toolbox.register("evaluate", cli.evaluate)
+    toolbox.register("mate", cli.crossOver)
+    toolbox.register("mutate", cli.mutate)
+
+    toolbox.register("select", tools.selNSGA2)
+
+    return toolbox
+
+
+def get_toolbox(frams_path, OPTIMIZATION_CRITERIA):
+    creator.create("FitnessMax", base.Fitness, weights=[1.0] * len(OPTIMIZATION_CRITERIA))
+    creator.create("Individual", list, fitness=creator.FitnessMax)
+
+    attributes = 1
+
+    cli = FramsticksLib(frams_path, None, None)
+
+    toolbox = register_standard(attributes, creator, cli)
+
+    return toolbox
