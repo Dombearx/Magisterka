392af5cbd0dfea05a0a7a1a5e0dd6fa08e383d49
Domin <domin.dober@gmail.com>
2021-10-02 16:37:44 +0200

    Remove prints

--- Statistics.py
+++ Statistics.py
@@ -5,9 +5,9 @@ def print_statistics(population: list, hall_of_fame: BasicParetoFront, iteration
     # for island in population:
     #     for ind in island:
     #         print(ind, f"{ind.fitness=}")
-    print(f"hall of fame in {iteration_number} iteration:")
+    print("hall of fame in", iteration_number, "iteration:")
     for ind in hall_of_fame:
-        print(ind, f"{ind.fitness=}")
+        print(ind, "fitness", ind.fitness)
 
 
 def print_statistics_removed(population: list, hall_of_fame: BasicParetoFront, iteration_number: int,
@@ -15,7 +15,8 @@ def print_statistics_removed(population: list, hall_of_fame: BasicParetoFront, i
     # for island in population:
     #     for ind in island:
     #         print(ind, f"{ind.fitness=}")
-    print(f"{removed} removed in {iteration_number} iteration:")
+    # print(f"{removed} removed in {iteration_number} iteration:")
+    print("removed", removed, "in iteration", iteration_number)
 
 
 def dont_print_statistics(population: list, hall_of_fame: BasicParetoFront, iteration_number: int, removed: int) -> None:
